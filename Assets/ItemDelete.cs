fc7584055b489d69ab1ae50e1ea877efd89b174e
megumi sugatani <ririaaa1126@gmail.com>
2018-01-01 23:08:02 +0900

    StepAsideUnityChanプロジェクト　3Dアクションゲームの作成
    課題の余計なオブジェクトの削除を実装したファイルです。

---
+++ Assets/ItemDelete.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDelete : MonoBehaviour {
+
+    private GameObject unitychan;
+
+	// Use this for initialization
+	void Start () {
+        //Unityちゃんのオブジェクトを取得
+        unitychan = GameObject.Find("unitychan");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.transform.position.z < unitychan.transform.position.z-7)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
